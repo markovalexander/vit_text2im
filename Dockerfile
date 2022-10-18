7acaea376cebf89b9bf38bde89372d94787a7d8a
ALexander Markov <almarkv@yandex.ru>
2022-10-18 14:21:53 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM nvcr.io/nvidia/pytorch:21.12-py3
+
+RUN set -x && \
+    echo "Acquire { HTTP { Proxy \"$HTTP_PROXY\"; }; };" | tee /etc/apt/apt.conf
+
+ENV LC_ALL=C.UTF-8 LANG=C.UTF-8
+
+RUN apt-get update || true && apt-get install -y build-essential \
+    wget curl git git-lfs vim zip unzip tmux htop
+
+RUN apt-get install -y libaio1 libaio-dev
+
+WORKDIR /workspace
+
+RUN pip install triton==1.0.0
+
+RUN DS_BUILD_QUANTIZER=1 DS_BUILD_TRANSFORMER_INFERENCE=1 DS_BUILD_TRANSFORMER=1 DS_BUILD_CPU_ADAM=1 DS_BUILD_FUSED_ADAM=1 DS_BUILD_FUSED_LAMB=1 DS_BUILD_SPARSE_ATTN=1 DS_BUILD_UTILS=1 DS_BUILD_AIO=1  pip install deepspeed --global-option="build_ext" --global-option="-j8"
+RUN pip install transformers datasets tokenizers pydantic pandas boto3 wandb numpy scikit-learn jupyter ipython evaluate accelerate sacrebleu nltk
+
+RUN python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('wordnet'); nltk.download('wordnet_ic'); nltk.download('sentiwordnet'); nltk.download('omw-1.4')"
+CMD sleep infinity
---
+++ src/models/vit_vqgan/layers.py
@@ -0,0 +1,50 @@
+from torch import Tensor, nn
+
+from src.data_types import TLayer
+
+
+class NormAndTransform(nn.Module):
+    def __init__(self, dim: int, layer: TLayer):
+        super().__init__()
+
+        self.layer = layer
+        self.layer_norm = nn.LayerNorm(dim)
+
+    def forward(self, x:Tensor, **kwargs) -> Tensor:
+        return self.layer(self.layer_norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: int):
+        super().__init__()
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, input_dim),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.feed_forward(x)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim: int, num_layers: int, num_heads: int, mlp_hidden_dim, dropout: float = 0):
+        super().__init__()
+        nn.MultiheadAttention()
+        self.layers  = []
+        for _ in range(num_layers):
+            transformer_layer = nn.ModuleList([
+                NormAndTransform(dim, nn.MultiheadAttention(embed_dim=dim, num_heads=num_heads, dropout=dropout)),
+                NormAndTransform(dim, FeedForward(dim, mlp_hidden_dim)),
+            ])
+            self.layers.append(transformer_layer)
+
+        self.layers = nn.ModuleList(self.layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        for attention_layer, feed_forward_layer in self.layers:
+            x = attention_layer(x) + x
+            x = feed_forward_layer(x) + x
+
+        return x
